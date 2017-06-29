dccb28d5556ef5e10352cfaed56a3c008fa42784
Felix Bruns <felixbruns@spotify.com>
2017-06-29 16:39:39 +0200

    tmp_avx

--- test/src/test_skip_chars.cpp
+++ test/src/test_skip_chars.cpp
@@ -15,6 +15,8 @@
  */
 
 #include <cstdlib>
+#include <type_traits>
+#include <iostream>
 
 #include <boost/mpl/list.hpp>
 #include <boost/test/unit_test.hpp>
@@ -27,6 +29,15 @@ BOOST_AUTO_TEST_SUITE(detail)
 
 namespace {
 
+struct cpu_ext_none {};
+struct cpu_ext_sse42 {};
+struct cpu_ext_avx2 {};
+
+using cpu_ext_list = boost::mpl::list<
+    cpu_ext_none,
+    cpu_ext_sse42,
+    cpu_ext_avx2>;
+
 std::string generate(const std::string &tpl, const std::size_t count) {
   std::string ws;
   ws.reserve(count);
@@ -36,14 +47,14 @@ std::string generate(const std::string &tpl, const std::size_t count) {
   return ws;
 }
 
-template <void (*function)(decode_context &)>
+template <void (*function)(decode_context &), typename cpu_ext>
 void verify_skip_any(
-    const bool use_sse,
     const std::string &json,
     const std::size_t prefix = 0,
     const std::size_t suffix = 0) {
   auto context = decode_context(json.data() + prefix, json.data() + json.size());
-  *const_cast<bool *>(&context.has_sse42) &= use_sse;
+  *const_cast<bool *>(&context.has_sse42) &= std::is_same<cpu_ext, cpu_ext_sse42>::value;
+  *const_cast<bool *>(&context.has_avx2) &= std::is_same<cpu_ext, cpu_ext_avx2>::value;
   const auto original_context = context;
   function(context);
   BOOST_CHECK_EQUAL(
@@ -54,16 +65,20 @@ void verify_skip_any(
       reinterpret_cast<intptr_t>(original_context.end));
 }
 
-template <void (*function)(decode_context &)>
-void verify_skip_empty_nullptr(const bool use_sse) {
+template <void (*function)(decode_context &), typename cpu_ext>
+void verify_skip_empty_nullptr() {
   auto context = decode_context(nullptr, nullptr);
-  *const_cast<bool *>(&context.has_sse42) &= use_sse;
+  std::cout << typeid(cpu_ext).name() << std::endl;
+  std::cout << std::is_same<cpu_ext, cpu_ext_sse42>::value << std::endl;
+  std::cout << std::is_same<cpu_ext, cpu_ext_avx2>::value << std::endl;
+  *const_cast<bool *>(&context.has_sse42) &= std::is_same<cpu_ext, cpu_ext_sse42>::value;
+  *const_cast<bool *>(&context.has_avx2) &= std::is_same<cpu_ext, cpu_ext_avx2>::value;
+  std::cout << context.has_sse42 << std::endl;
+  std::cout << context.has_avx2 << std::endl;
   function(context);
   BOOST_CHECK(context.position == nullptr);
   BOOST_CHECK(context.end == nullptr);
 }
-
-using true_false = boost::mpl::list<boost::true_type, boost::false_type>;
 
 }  // namespace
 
@@ -71,84 +86,84 @@ using true_false = boost::mpl::list<boost::true_type, boost::false_type>;
  * skip_any_simple_characters
  */
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_simple_characters, use_sse, true_false) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_simple_characters, cpu_ext, cpu_ext_list) {
   for (auto n = 0; n < 1024; n++) {
     const auto ws = generate("abcdefghIJKLMNOP:-,;'^¨´`xyz", n);
     const auto with_prefix = "\\" + ws;
     const auto with_suffix = ws + "\"abcde";
-    verify_skip_any<skip_any_simple_characters>(use_sse::value, ws);
-    verify_skip_any<skip_any_simple_characters>(use_sse::value, with_prefix, 1);
-    verify_skip_any<skip_any_simple_characters>(use_sse::value, with_suffix, 0, 6);
+    verify_skip_any<skip_any_simple_characters, cpu_ext>(ws);
+    verify_skip_any<skip_any_simple_characters, cpu_ext>(with_prefix, 1);
+    verify_skip_any<skip_any_simple_characters, cpu_ext>(with_suffix, 0, 6);
   }
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_simple_characters_with_empty_string,
-                              use_sse,
-                              true_false) {
-  verify_skip_empty_nullptr<skip_any_simple_characters>(use_sse::value);
+                              cpu_ext,
+                              cpu_ext_list) {
+  verify_skip_empty_nullptr<skip_any_simple_characters, cpu_ext>();
 }
 
 /*
  * skip_any_whitespace
  */
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_space, use_sse, true_false) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_space, cpu_ext, cpu_ext_list) {
   for (auto n = 0; n < 1024; n++) {
     const auto ws = generate(" ", n);
     const auto with_prefix = "}" + ws;
     const auto with_suffix = ws + "{ ";
-    verify_skip_any<skip_any_whitespace>(use_sse::value, ws);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_prefix, 1);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_suffix, 0, 2);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(ws);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_prefix, 1);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_suffix, 0, 2);
   }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_tabs, use_sse, true_false) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_tabs, cpu_ext, cpu_ext_list) {
   for (auto n = 0; n < 1024; n++) {
     const auto ws = generate("\t", n);
     const auto with_prefix = "}" + ws;
     const auto with_suffix = ws + "{ ";
-    verify_skip_any<skip_any_whitespace>(use_sse::value, ws);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_prefix, 1);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_suffix, 0, 2);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(ws);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_prefix, 1);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_suffix, 0, 2);
   }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_carriage_return, use_sse, true_false) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_carriage_return, cpu_ext, cpu_ext_list) {
   for (auto n = 0; n < 1024; n++) {
     const auto ws = generate("\r", n);
     const auto with_prefix = "}" + ws;
     const auto with_suffix = ws + "{ ";
-    verify_skip_any<skip_any_whitespace>(use_sse::value, ws);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_prefix, 1);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_suffix, 0, 2);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(ws);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_prefix, 1);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_suffix, 0, 2);
   }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_line_feed, use_sse, true_false) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_line_feed, cpu_ext, cpu_ext_list) {
   for (auto n = 0; n < 1024; n++) {
     const auto ws = generate("\n", n);
     const auto with_prefix = "}" + ws;
     const auto with_suffix = ws + "{ ";
-    verify_skip_any<skip_any_whitespace>(use_sse::value, ws);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_prefix, 1);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_suffix, 0, 2);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(ws);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_prefix, 1);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_suffix, 0, 2);
   }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_whitespace, use_sse, true_false) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_whitespace, cpu_ext, cpu_ext_list) {
   for (auto n = 0; n < 1024; n++) {
     const auto ws = generate("\n\t\r\n", n);
     const auto with_prefix = "}" + ws;
     const auto with_suffix = ws + "{ ";
-    verify_skip_any<skip_any_whitespace>(use_sse::value, ws);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_prefix, 1);
-    verify_skip_any<skip_any_whitespace>(use_sse::value, with_suffix, 0, 2);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(ws);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_prefix, 1);
+    verify_skip_any<skip_any_whitespace, cpu_ext>(with_suffix, 0, 2);
   }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_whitespace_with_empty_string, use_sse, true_false) {
-  verify_skip_empty_nullptr<skip_any_whitespace>(use_sse::value);
+BOOST_AUTO_TEST_CASE_TEMPLATE(json_skip_any_whitespace_with_empty_string, cpu_ext, cpu_ext_list) {
+  verify_skip_empty_nullptr<skip_any_whitespace, cpu_ext>();
 }
 
 BOOST_AUTO_TEST_SUITE_END()  // detail
